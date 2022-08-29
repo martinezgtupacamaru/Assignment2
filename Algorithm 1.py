28d064c3b0a999e5cd365d8606ffc0f55271c61c
GM <guillermo_f50@hotmail.com>
2022-08-28 23:51:07 -0400

    commit_yes

--- Algorithm 1.py
+++ Algorithm 1.py
@@ -1,9 +1,8 @@
-# ##Problem 2
-# def func (n):
-#     for x in range(n):
-#         if 2**x >= n:
-#             return 2**x
-#
-# print(func(5))
+##Problem 2
+def func (n):
+    for x in range(n):
+        if 2**x >= n:
+            return 2**x
 
-print(5 == int)
+print(func(5))
+
